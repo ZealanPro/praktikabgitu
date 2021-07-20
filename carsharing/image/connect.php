2ddb95d9089e663e3ff7d96387f4bf577859d23c
skillreturn <lodbipb6@gmail.com>
2021-07-21 01:58:34 +0300

    Delete carsharing/image directory

--- carsharing/image/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost';
-$user = 'root';
-$password = 'root';
-$db = 'carsharing';
-	
-$link = mysqli_connect($host,$user,$password,$db);
-echo "Соединение с базой данных carsharing установлено!";
-?>
